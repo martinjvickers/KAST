afe685c3a9042df40e30cf2c4c4809e70dd93a5a
Martin Vickers <rudigarude@googlemail.com>
2016-09-07 12:45:57 +0100

    Renamed "new" functions to move them into production

--- utils.h
+++ utils.h
@@ -1,9 +1,9 @@
 Dna getRevCompl(Dna const & nucleotide);
 Dna5String doRevCompl(Dna5String seq);
 String<Dna5String> defineKmers(int kmerlength);
-void newcount(Dna5String seq, int klen, unordered_map<string, long long int> & map);
+void count(Dna5String seq, int klen, unordered_map<string, long long int> & map);
 void count(String<Dna5String> kmers, Dna5String seq, int klen, int counts[]);
 void count(String<Dna5String> kmers, Dna5String seq, int klen, long long int counts[]);
-void newmarkov(Dna5String seq, int klen, int markovOrder, unordered_map<string,thingy> & markovthingy);
+void markov(Dna5String seq, int klen, int markovOrder, unordered_map<string,thingy> & markovthingy);
 void markov(String<Dna5String> kmers, Dna5String seq, int klen, long long int counts[], int markovOrder, double kmerProb[]);
 void recordall(int nohits, double hits[], double value, int seqcurrpos, int hitpos[]);
