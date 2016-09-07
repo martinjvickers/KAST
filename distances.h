2efbe224ce4a4baf8eccd1a6e47683046e1efb59
Martin Vickers (JIC) <mvickers@n108379.nbi.ac.uk>
2016-09-07 12:45:07 +0100

    Adding d2star distance measure and removed old versions of kmer/euler/d2s/d2star

--- distances.h
+++ distances.h
@@ -1,9 +1,6 @@
 #include <math.h>
 
-double d2(long long int ref[], long long int qry[], int nokmers);
-double newd2(unordered_map<string, long long int> refmap, unordered_map<string, long long int> querymap);
-double neweuler(unordered_map<string, long long int> refmap, unordered_map<string, long long int> querymap);
-double euler(long long int ref[], long long int qry[], int nokmers);
-double newd2s(unordered_map<string,thingy> qrymarkovthingy, unordered_map<string,thingy> refmarkovthingy);
-double d2s(long long int refCounts[], long long int qryCounts[], int nokmers, double refProbs[], double qryProbs[]);
-double d2star(long long int refCounts[], long long int qryCounts[], int nokmers, double refProbs[], double qryProbs[]);
+double d2(unordered_map<string, long long int> refmap, unordered_map<string, long long int> querymap);
+double euler(unordered_map<string, long long int> refmap, unordered_map<string, long long int> querymap);
+double d2s(unordered_map<string,thingy> qrymarkovthingy, unordered_map<string,thingy> refmarkovthingy);
+double d2star(unordered_map<string,thingy> qrymarkovthingy, unordered_map<string,thingy> refmarkovthingy);
