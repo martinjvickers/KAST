073e61585bd1288fd5c2cd14d78e0006576096c6
Martin Vickers (JIC) <mvickers@n108379.nbi.ac.uk>
2016-09-19 14:49:04 +0100

    Precomputing reference and storing in memory is an option using the -n flag

--- utils.h
+++ utils.h
@@ -25,7 +25,11 @@ SOFTWARE.
 
 Iupac getRevCompl(Iupac const & nucleotide);
 Dna5String doRevCompl(Dna5String seq);
-//String<Dna5String> defineKmers(int kmerlength);
 void count(Dna5String seq, int klen, unordered_map<string, long long int> & map);
 void markov(Dna5String seq, int klen, int markovOrder, unordered_map<string,markov_dat> & markovmap);
 void recordall(int nohits, double hits[], double value, int seqcurrpos, int hitpos[]);
+void gettophits(ModifyStringOptions options, unordered_map<string, long long int> query_countmap, CharString queryid);
+void gettophits(ModifyStringOptions options, unordered_map<string, markov_dat> query_markovmap, CharString queryid);
+
+void gettophits(ModifyStringOptions options, unordered_map<string, markov_dat> query_markovmap, CharString queryid, vector<unordered_map<string,markov_dat>> reference_markov_vec);
+void gettophits(ModifyStringOptions options, unordered_map<string, long long int> query_countsmap, CharString queryid, vector<unordered_map<string,long long int>> reference_counts_vec);
