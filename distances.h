d61fdb39c05c973c211af813fdd001d21e610877
Martin Vickers <rudigarude@googlemail.com>
2016-09-28 10:48:53 +0100

    Remove old d2star distance

--- distances.h
+++ distances.h
@@ -28,7 +28,7 @@ SOFTWARE.
 double d2(unordered_map<string, long long int> refmap, unordered_map<string, long long int> querymap);
 double euler(unordered_map<string, long long int> refmap, unordered_map<string, long long int> querymap);
 double d2s(unordered_map<string,markov_dat> qrymarkovthingy, unordered_map<string,markov_dat> refmarkovthingy);
-double d2star_check(unordered_map<string,markov_dat> qrymarkovthingy, unordered_map<string,markov_dat> refmarkovthingy);
+//double d2star_check(unordered_map<string,markov_dat> qrymarkovthingy, unordered_map<string,markov_dat> refmarkovthingy);
 double d2star(unordered_map<string,markov_dat> qrymarkovthingy, unordered_map<string,markov_dat> refmarkovthingy);
 
 double hao(unordered_map<string,markov_dat> qrymarkovthingy, unordered_map<string,markov_dat> refmarkovthingy);
