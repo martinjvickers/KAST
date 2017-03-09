2c28554b9697a485d3426248600d25be00e4c5e6
Martin Vickers <rudigarude@googlemail.com>
2017-03-09 10:58:54 +0000

    Added hao calc kmers

--- alfsc.cpp
+++ alfsc.cpp
@@ -471,7 +471,7 @@ int main(int argc, char const ** argv)
 		if(options.type == "d2s-opt")
 		{
 			kmermap = makeall(options);
-		} else if(options.type == "d2s" || options.type == "d2star") //else here we just go ahead and make all of the kmers available.
+		} else if(options.type == "d2s" || options.type == "d2star" || options.type == "hao") //else here we just go ahead and make all of the kmers available.
 		{
 			kmermap = makecomplete(options);
 		}
