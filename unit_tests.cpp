3e6ac6e2f9bc9dbd8f81804472ce1efbf6051ddb
Martin Vickers <rudigarude@googlemail.com>
2017-04-05 15:30:15 +0100

    Added distance unit tests

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -29,19 +29,120 @@ SOFTWARE.
 #include "distance.h"
 #include "utils.h"
 
+/*
+Manhattan 0.63265
+Ch 0.03061
+*/
+
+
+
+int testd2star(){
+
+        int klen = 3;
+        int markovOrder = 1;
+        IupacString qryseq = doRevCompl("AGGCAGCGTACGAACCTACTGGAGTTGCGGTATGGGACCAGGCGACCTCTGATGCAGAGATACAGGAGCGCCGCGCCGGGTCTTCCTTGTAGAAGTCCTG");
+        IupacString refseq = doRevCompl("CGGAGACCTCCGTGGACGGGGAAGTCCTGCGCGGGTCAGACGTACGCCCCGATTAGTTGCCCGGACGCCCGGTTGGCAGAAGTGACGGCGACTGCCCTCA");
+
+        ModifyStringOptions options;
+        options.klen = klen;
+        options.markovOrder = markovOrder;
+        map<string, unsigned int> refcounts = count(refseq, klen);
+        map<string, unsigned int> querycounts = count(qryseq, klen);
+        map<string, bool> ourkmers = makecomplete(options);
+        map<string, double> refmarkov = markov(klen, refseq, markovOrder, ourkmers);
+        map<string, double> querymarkov = markov(klen, qryseq, markovOrder, ourkmers);
+
+        double dist = d2star(options, ourkmers, refcounts, refmarkov, querycounts, querymarkov);
+        double expected = 0.4027100011247771;
+
+        double epsilon = 0.000001;
+        if(abs(dist - expected) < epsilon)
+        {
+                return 0;
+        } else {
+                printf("Test Euler FAILED: Value expected=%1.15f, but recieved=%1.15f \n", expected, dist);
+                return 1;
+        }
+
+        return 0;
+}
+
+int testd2s(){
+
+        int klen = 3;
+	int markovOrder = 1;
+        IupacString qryseq = doRevCompl("AGGCAGCGTACGAACCTACTGGAGTTGCGGTATGGGACCAGGCGACCTCTGATGCAGAGATACAGGAGCGCCGCGCCGGGTCTTCCTTGTAGAAGTCCTG");
+        IupacString refseq = doRevCompl("CGGAGACCTCCGTGGACGGGGAAGTCCTGCGCGGGTCAGACGTACGCCCCGATTAGTTGCCCGGACGCCCGGTTGGCAGAAGTGACGGCGACTGCCCTCA");
+
+        ModifyStringOptions options;
+	options.klen = klen;
+	options.markovOrder = markovOrder;
+        map<string, unsigned int> refcounts = count(refseq, klen);
+        map<string, unsigned int> querycounts = count(qryseq, klen);
+	map<string, bool> ourkmers = makecomplete(options);
+	map<string, double> refmarkov = markov(klen, refseq, markovOrder, ourkmers);
+	map<string, double> querymarkov = markov(klen, qryseq, markovOrder, ourkmers);
+
+	double dist = d2s(options, ourkmers, refcounts, refmarkov, querycounts, querymarkov);
+        double expected = 0.432463894423;
+	
+        double epsilon = 0.000001;
+        if(abs(dist - expected) < epsilon)
+        {
+                return 0;
+        } else {
+                printf("Test Euler FAILED: Value expected=%1.15f, but recieved=%1.15f \n", expected, dist);
+                return 1;
+        }
+
+        return 0;
+}
+
+int testchebyshev(){
+
+        int klen = 3;
+        IupacString qryseq = doRevCompl("AGGCAGCGTACGAACCTACTGGAGTTGCGGTATGGGACCAGGCGACCTCTGATGCAGAGATACAGGAGCGCCGCGCCGGGTCTTCCTTGTAGAAGTCCTG");
+        IupacString refseq = doRevCompl("CGGAGACCTCCGTGGACGGGGAAGTCCTGCGCGGGTCAGACGTACGCCCCGATTAGTTGCCCGGACGCCCGGTTGGCAGAAGTGACGGCGACTGCCCTCA");
+
+        ModifyStringOptions options;
+        map<string, unsigned int> refcounts = count(refseq, klen);
+        map<string, unsigned int> querycounts = count(qryseq, klen);
+
+        double dist = chebyshev(options, refcounts, querycounts);
+        double expected = 0.03061;
+        double epsilon = 0.00001;
+        if(abs(dist - expected) < epsilon)
+        {
+                return 0;
+        } else {
+                printf("Test Chebyshev FAILED: Value expected=%1.15f, but recieved=%1.15f \n", expected, dist);
+                return 1;
+        }
+
+        return 0;
+}
+
+
 int testeuler(){
 
 	int klen = 3;
-	IupacString qryseq = doRevCompl("GATTGCCTCTCATTTTCTCTCCCATATTATAGGGTGAAATATGATCGCGTATGCGAGAGTAGTGCCAACATATTGTGATCTTCGATTTTTTGGCAACCCAAAATGGAGGCGGACGAACGAGATGATAATGATAAGATGATTCAAAAAGACAATGCACGACAGAGAGAGCAGAAAAGATAA");
-	IupacString refseq = doRevCompl("CCACACCACACCCACACACCCACACACCACACCACACACCACACCACACCCACACACACACATCCTAACACTACCCTAACACAGCCCTAATCTAACCCTGGCCAACCTGTCTCTCAACTTACCCTCCATTACCCTGCCTCCACTCGTTACCCTGTCCCATTCAACCATACCACTCCGAAC");
-	
+	IupacString qryseq = doRevCompl("AGGCAGCGTACGAACCTACTGGAGTTGCGGTATGGGACCAGGCGACCTCTGATGCAGAGATACAGGAGCGCCGCGCCGGGTCTTCCTTGTAGAAGTCCTG");
+	IupacString refseq = doRevCompl("CGGAGACCTCCGTGGACGGGGAAGTCCTGCGCGGGTCAGACGTACGCCCCGATTAGTTGCCCGGACGCCCGGTTGGCAGAAGTGACGGCGACTGCCCTCA");
+
 	ModifyStringOptions options;
 	map<string, unsigned int> refcounts = count(refseq, klen);
 	map<string, unsigned int> querycounts = count(qryseq, klen);
 	
 	double dist = euler(options, refcounts, querycounts);
-
-	cout << "Euler " << dist << endl;
+	double expected = 0.103056;
+	double epsilon = 0.000001;
+	if(abs(dist - expected) < epsilon)
+        {
+                return 0;
+        } else {
+                printf("Test Euler FAILED: Value expected=%1.15f, but recieved=%1.15f \n", expected, dist);
+                return 1;
+        }
 
 	return 0;
 }
@@ -49,33 +150,49 @@ int testeuler(){
 int testd2(){
 
         int klen = 3;
-        IupacString qryseq = doRevCompl("GATTGCCTCTCATTTTCTCTCCCATATTATAGGGTGAAATATGATCGCGTATGCGAGAGTAGTGCCAACATATTGTGATCTTCGATTTTTTGGCAACCCAAAATGGAGGCGGACGAACGAGATGATAATGATAAGATGATTCAAAAAGACAATGCACGACAGAGAGAGCAGAAAAGATAA");
-        IupacString refseq = doRevCompl("CCACACCACACCCACACACCCACACACCACACCACACACCACACCACACCCACACACACACATCCTAACACTACCCTAACACAGCCCTAATCTAACCCTGGCCAACCTGTCTCTCAACTTACCCTCCATTACCCTGCCTCCACTCGTTACCCTGTCCCATTCAACCATACCACTCCGAAC");
+        IupacString qryseq = doRevCompl("AGGCAGCGTACGAACCTACTGGAGTTGCGGTATGGGACCAGGCGACCTCTGATGCAGAGATACAGGAGCGCCGCGCCGGGTCTTCCTTGTAGAAGTCCTG");
+        IupacString refseq = doRevCompl("CGGAGACCTCCGTGGACGGGGAAGTCCTGCGCGGGTCAGACGTACGCCCCGATTAGTTGCCCGGACGCCCGGTTGGCAGAAGTGACGGCGACTGCCCTCA");
 
         ModifyStringOptions options;
         map<string, unsigned int> refcounts = count(refseq, klen);
         map<string, unsigned int> querycounts = count(qryseq, klen);
 
         double dist = d2(options, refcounts, querycounts);
+	double expected = 0.10619;
+	double epsilon = 0.000001;
+	if(abs(dist - expected) < epsilon)
+	{
+		return 0;
+	} else {
+		printf("Test d2 FAILED: Value expected=%1.15f, but recieved=%1.15f \n", expected, dist);
+		return 1;
+	}
 
-        cout << "d2 " << dist << endl;
-
-        return 0;
+	return 0;
 }
 
 int testmanhattan(){
 
         int klen = 3;
-        IupacString qryseq = doRevCompl("GATTGCCTCTCATTTTCTCTCCCATATTATAGGGTGAAATATGATCGCGTATGCGAGAGTAGTGCCAACATATTGTGATCTTCGATTTTTTGGCAACCCAAAATGGAGGCGGACGAACGAGATGATAATGATAAGATGATTCAAAAAGACAATGCACGACAGAGAGAGCAGAAAAGATAA");
-        IupacString refseq = doRevCompl("CCACACCACACCCACACACCCACACACCACACCACACACCACACCACACCCACACACACACATCCTAACACTACCCTAACACAGCCCTAATCTAACCCTGGCCAACCTGTCTCTCAACTTACCCTCCATTACCCTGCCTCCACTCGTTACCCTGTCCCATTCAACCATACCACTCCGAAC");
+        IupacString qryseq = doRevCompl("AGGCAGCGTACGAACCTACTGGAGTTGCGGTATGGGACCAGGCGACCTCTGATGCAGAGATACAGGAGCGCCGCGCCGGGTCTTCCTTGTAGAAGTCCTG");
+        IupacString refseq = doRevCompl("CGGAGACCTCCGTGGACGGGGAAGTCCTGCGCGGGTCAGACGTACGCCCCGATTAGTTGCCCGGACGCCCGGTTGGCAGAAGTGACGGCGACTGCCCTCA");
 
         ModifyStringOptions options;
         map<string, unsigned int> refcounts = count(refseq, klen);
         map<string, unsigned int> querycounts = count(qryseq, klen);
 
         double dist = manhattan(options, refcounts, querycounts);
+	double expected = 0.63265;
+	double epsilon = 0.000001;
 
-        cout << "Manhattan " << dist << endl;
+	if(abs(dist - expected) < epsilon)
+        {
+		printf("Test Manhattan PASSED: Value expected=%1.15f, but recieved=%1.15f \n", expected, dist);
+                return 0;
+        } else {
+		printf("Test Manhattan FAILED: Value expected=%1.15f, but recieved=%1.15f \n", expected, dist);
+                return 1;
+        }
 
         return 0;
 }
@@ -231,7 +348,7 @@ int main(int argc, char const ** argv)
         }
         else
         {
-                cout << "Test Euler PASSED" << endl;
+                cout << "Test d2 PASSED" << endl;
         }
 
         if(testmanhattan() != 0)
@@ -240,7 +357,34 @@ int main(int argc, char const ** argv)
         }
         else
         {
-                cout << "Test Euler PASSED" << endl;
+                cout << "Test Manhattan PASSED" << endl;
+        }
+
+	if(testd2s() != 0)
+        {
+                returncode = 1;
+        }
+        else
+        {
+                cout << "Test d2s PASSED" << endl;
+        }
+
+	if(testd2star() != 0)
+        {
+                returncode = 1;
+        }
+        else
+        {
+                cout << "Test d2star PASSED" << endl;
+        }
+
+	if(testchebyshev() != 0)
+        {
+                returncode = 1;
+        }
+        else
+        {
+                cout << "Test Chebyshev PASSED" << endl;
         }
 
 	return returncode;
