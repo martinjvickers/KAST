7ff49d27065e56520a334ba46d4622c317a9f415
Martin Vickers <rudigarude@googlemail.com>
2018-04-04 11:24:28 +0100

    Style and remove old code

--- utils.h
+++ utils.h
@@ -1,40 +1,26 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-/*
-#include <seqan/seq_io.h>
-#include <iostream>
-#include <seqan/sequence.h>  // CharString, ...
-#include <seqan/stream.h>    // to stream a CharString into cout
-#include <seqan/file.h>
-#include <seqan/arg_parse.h>
-#include <seqan/seq_io.h>
-#include <math.h>      
-#include <seqan/store.h>
-#include <string>
-#include <thread>
-#include <map>
-#include <vector>
-*/
-
 #include "common.h"
 
 #include <seqan/seq_io.h>
 #include <seqan/arg_parse.h>
 
-seqan::ArgumentParser::ParseResult parseCommandLine(ModifyStringOptions & options, int argc, char const ** argv);
+ArgumentParser::ParseResult parseCommandLine(ModifyStringOptions & options, 
+                                             int argc, char const ** argv);
 AminoAcid getRevCompl(AminoAcid const & nucleotide);
-//Dna5String doRevCompl(Dna5String seq);
 String<AminoAcid> doRevCompl(String<AminoAcid> seq);
 map<string, unsigned int> count(String<AminoAcid> sequence, int klen);
-map<string, unsigned int> count(String<AminoAcid> sequence, int klen, vector<CharString> mask);
+map<string, unsigned int> count(String<AminoAcid> sequence, int klen, 
+                                vector<CharString> mask);
 double gc_ratio(String<AminoAcid> sequence);
-map<string, double> markov(int klen, String<AminoAcid> sequence, int markovOrder, map<string, bool> kmer_count_map);
+map<string, double> markov(int klen, String<AminoAcid> sequence, 
+                           int markovOrder, map<string, bool> kmer_count_map);
 map<string, bool> makecomplete(ModifyStringOptions options);
-map<string, bool> makequick(ModifyStringOptions options, StringSet<String<AminoAcid>> referenceseqs);
-//map<string, bool> makequick(ModifyStringOptions options, StringSet<Dna5String> referenceseqs);
-//int makequick(ModifyStringOptions options, StringSet<Dna5String> referenceseqs, map<string, bool> quickkmers);
+map<string, bool> makequick(ModifyStringOptions options, 
+                            StringSet<String<AminoAcid>> referenceseqs);
 int ipow(int base, int exp);
 CharString namecut(CharString seq, int val);
 int parseMask(ModifyStringOptions options, int &effectiveKlen);
+
 #endif
