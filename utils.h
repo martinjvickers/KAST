d7032b457877056f12b279cc66d0cdcacb2a9cdb
Martin Vickers <rudigarude@googlemail.com>
2019-02-26 10:24:53 +0000

    Bump version and date

--- utils.h
+++ utils.h
@@ -82,8 +82,8 @@ ArgumentParser::ParseResult parseCommandLine(ModifyStringOptions & options,
               longer."));*/
    setDefaultValue(parser, "num-cores", "1");
    setShortDescription(parser, "Kmer Alignment-free Search Tool.");
-   setVersion(parser, "0.0.26");
-   setDate(parser, "January 2019");
+   setVersion(parser, "0.0.27");
+   setDate(parser, "February 2019");
    addUsageLine(parser, "-q query.fasta -r reference.fasta -o results.txt [\\fIOPTIONS\\fP] ");
    addUsageLine(parser, "-p mydata.fasta -o results.txt [\\fIOPTIONS\\fP] ");
    addDescription(parser, "Perform Alignment-free k-tuple frequency \
